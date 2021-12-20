cf9270b3bcef3d316f6dda83068a5ab2f079d16c
Ibrahim <ibrahimbsc8@gmail.com>
2021-12-20 12:44:39 -0800

    redone

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,29 +1,16 @@
 #!/usr/bin/python3
-"""module evaluate geometry using maths operations"""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
-
-class Rectangle(BaseGeometry):
-    """Rectangle class inheriting from BaseGeometry class"""
-    def __init__(self, width, height):
-        """initializes new object of Rectangle Class"""
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
-        self.__height = height
-        self.__width = width
-
-    def area(self):
-        return self.__height * self.__width
-
-    def __str__(self):
-        return "[{}] {:d}/{:d}".format("Rectangle", self.__width,
-                                       self.__height)
+"""Defines a Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Square class inheriting from Rectangle class"""
+    """Represent a square."""
+
     def __init__(self, size):
-        """initializes new object of Rectangle Class"""
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
-        super().__init__(self.__size, self.__size)
